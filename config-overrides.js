3015fd0bd22c55547cb002e75f1d3d1cff2cb909
Alexander Ivanov <aivanov@informatica.ru>
2018-05-11 15:49:22 +0300

    client_build

--- config-overrides.js
+++ config-overrides.js
@@ -1,20 +1,24 @@
 /* config-overrides.js */
 const { injectBabelPlugin } = require('react-app-rewired');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+const paths = require('react-scripts/config/paths');
 const path = require('path');
 const fs = require('fs');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(['syntax-dynamic-import'], config);
+  console.log('NODE_ENV', env);
   if (env === 'production') {
     config.devtool = 'nosources-source-map'; //без sourcemap
   }
   config.resolve.alias = {
-    'deql': path.resolve(__dirname, './src/'),
+    deql: path.resolve(__dirname, './src/'),
     '@': path.resolve(__dirname, './../'),
   };
 
   delete config.resolve.plugins[0];
-  config.resolve.plugins[0] = new ModuleScopePlugin(path.resolve(__dirname,'./../'))
+  config.resolve.plugins[0] = new ModuleScopePlugin(
+    path.resolve(__dirname, './../')
+  );
   return config;
 };
